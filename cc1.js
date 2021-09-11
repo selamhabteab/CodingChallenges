92a7ddf6e00fd58dca44518294b4f0f7c41195d6
Selam Habteab <selamhabteab@Selams-MBP.attlocal.net>
2021-09-11 16:08:28 -0700

    submitted day 2 of coding challenges

--- cc1.js
+++ cc1.js
@@ -6,11 +6,11 @@ function reverseString (string) {
     let tempArr = string.split('').reverse()
     return tempArr.join('')
 }
-console.log(reverseString('hello'));
+console.log('original version: ',reverseString('hello'));
 
 //short version:
 const reverseStr = (string) => tempArr = string.split('').reverse().join('')
-console.log(reverseStr('hello'));
+console.log('short version: ', reverseStr('hello'));
 
 //using a for loop 
 function reverseForLoop (string) {
@@ -20,6 +20,26 @@ function reverseForLoop (string) {
     }
     return tempStr
 }
-console.log(reverseForLoop('hello'));
+console.log('For Loop version: ',reverseForLoop('hello'));
 //------------------------------------
+
 // Day 2 - 9/11 - Factorialize
+
+//Return the factorial of the provided integer. If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+//factorials are often represented with the shorthand notation "n!" For example: 5! = 1 * 2* 3 * 4 * 5 = 120
+
+//My logic: 
+//Create a function
+//Pass in an argument for 'n'
+//declare a variable to catch each iteration of the for loop
+//Create a for loop that counts down i--
+//Reassign the variable with a multiplication assignment operator
+function factorial (n) {
+    let solution = 1
+    for (let i=n; i>0; i--) {
+        solution *= i;
+    }
+    return solution
+}
+console.log('factorial: ',factorial(5));
+
