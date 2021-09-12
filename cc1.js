0e6cef745dbb2deaddde5b0725162c5a093fe612
Selam Habteab <selamhabteab@Selams-MBP.attlocal.net>
2021-09-12 09:18:21 -0700

    completed day 3 of coding challenges

--- cc1.js
+++ cc1.js
@@ -1,6 +1,6 @@
 // Week 1
 
-// Day 1 - 9/10 - Reverse a string
+// DAY 1 - 9/10 - Reverse a string
 //My logic: 1)split string into an array 2)reverse order of array 3)return array into a string with .join("")
 function reverseString (string) {
     let tempArr = string.split('').reverse()
@@ -23,7 +23,7 @@ function reverseForLoop (string) {
 console.log('For Loop version: ',reverseForLoop('hello'));
 //------------------------------------
 
-// Day 2 - 9/11 - Factorialize
+// DAY 2 - 9/11 - Factorialize
 
 //Return the factorial of the provided integer. If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
 //factorials are often represented with the shorthand notation "n!" For example: 5! = 1 * 2* 3 * 4 * 5 = 120
@@ -43,3 +43,33 @@ function factorial (n) {
 }
 console.log('factorial: ',factorial(5));
 
+//------------------------------------
+
+// DAY 3 - 9/12 - Factorialize
+
+// Check for Palindrome. Return true if the given string is a palidrome. Otherwise, return false. 
+//My logic (pseudocode): 
+//Create a function that takes in a string
+//assign a variable to hold: split string into an array 
+//create a conditional that checks if that variable matches with it in reverse. 
+
+function palindrome (string) {
+    let string1 = string
+    let string2 = string.split('').reverse().join('')
+    console.log(`reversed string: `, string2);
+    console.log(`original string: `, string1);
+    if (string1 === string2) {
+        return true
+    }
+    return false
+}
+console.log(palindrome("racecar"));
+
+//After working on the function i learned I could shorten the function. 
+function pali (string) {
+    let reversed = string.split('').reverse().join('')
+    if (reversed === string) return true;
+    return false
+}
+console.log(pali("racecar")); //true
+console.log(pali("selam")); //false
