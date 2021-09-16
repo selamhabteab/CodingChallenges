5b4bbd8f82a4826bbdbd9840ac618005c055a749
Selam Habteab <selamhabteab@Selams-MBP.attlocal.net>
2021-09-15 17:44:06 -0700

    Day 6 of Coding Challenges

--- cc1.js
+++ cc1.js
@@ -112,5 +112,72 @@ for (let fruit of fruits){
 //My logic (pseudocode): 
 //Create a function that takes in a string "this is a string"
 //Assign a variable to hold: split string into an array 
+//Apply a for loop to iterate through the array and use substring method to isolate the first letter of every value: string[i].substring(0,1)
+//Apply uppercase method to the substring
+//then join array to return to sentence 
     //["this", "is", "a", "string"]. --> ["t", "h", "i"...]
-//nested for loop to extract first letter of each value and uppercase then join to return to string then join again to return to sentence 
+
+function titleCase (string) {
+    sentence = string.split(" "); //-->["this", "is", "a", "string"]
+    cap = []
+    for (let i=0; i<sentence.length; i++){
+        sentence[i].substring(0,1).toUpperCase() 
+        // console.log(sentence[i].substring(0,1).toUpperCase());
+            //extracts t, i, a, s --> T,I,A,S 
+                //Doesn't work because you cant return extracted letter 
+    }
+    return sentence.join(" ")
+}
+
+// console.log(titleCase("This is a string."));
+
+// Solution
+//Create a function that takes in a string "this is a string"
+//Assign a variable to hold: split string into an array 
+//Apply a for loop to iterate through the array 
+//Apply uppercase method to the first letter of every value 
+//return array to a sentence with .join(" ")
+
+function capitalized (string) {
+    wordsArr = string.split(" "); //-->["this", "is", "a", "string"]
+    console.log("words array: ", wordsArr);
+    capitalizedSentence = []
+    
+    for (let i=0; i<wordsArr.length; i++){
+        let substring = wordsArr[i].substring(1)
+        let firstLetter = wordsArr[i][0].toUpperCase()
+        let word = firstLetter + substring
+        capitalizedSentence.push(word)
+    };
+
+    console.log("capitalized sentence: ", capitalizedSentence);
+    
+    return capitalizedSentence.join(" ")
+}
+
+console.log(capitalized("This is a string."));
+
+//VIDEO SOLUTION
+// function solution (string) {
+//     let words = string.toLowerCase().split(" ");
+//     for (let i=0; i<words.length; i++){
+//         words[i] = words[i][0].toUpperCase() + words[i].slice(1)
+//     }
+//     return words.join(" ")
+// }
+
+//------------------------------------
+
+// DAY 6 - 9/14 - Largest numbers in Arrays
+
+// Return an array consisting of the largest number from each provided sub-array. 
+
+//My logic (pseudocode): 
+//Create a function that takes in an array 
+    //[[4,5,1], [13,2,7], [9,10,1], [500,5,33]]
+//Assign a variable to an empty array that will hold the largest numbers
+//Apply a for loop to iterate through the array 
+//add a conditional to extract the largest number in each array[i]
+//push the longest numbers into the empty array
+
+
